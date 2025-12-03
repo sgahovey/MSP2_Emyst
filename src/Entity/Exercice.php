756e223bb400899199b7bc76be0e5ff00c72b989
AHOVEY Saint-George <sgahovey@gmail.com>
2025-12-03 13:29:46 +0400

    Création de l'entité "exercice"

---
+++ src/Entity/Exercice.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ExerciceRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ExerciceRepository::class)]
+class Exercice
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $imageUrl = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $nom = null;
+
+    #[ORM\Column]
+    private ?int $repetitions = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $charge = null;
+
+    #[ORM\Column(type: Types::TIME_IMMUTABLE)]
+    private ?\DateTimeImmutable $duree = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getImageUrl(): ?string
+    {
+        return $this->imageUrl;
+    }
+
+    public function setImageUrl(?string $imageUrl): static
+    {
+        $this->imageUrl = $imageUrl;
+
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(string $nom): static
+    {
+        $this->nom = $nom;
+
+        return $this;
+    }
+
+    public function getRepetitions(): ?int
+    {
+        return $this->repetitions;
+    }
+
+    public function setRepetitions(int $repetitions): static
+    {
+        $this->repetitions = $repetitions;
+
+        return $this;
+    }
+
+    public function getCharge(): ?int
+    {
+        return $this->charge;
+    }
+
+    public function setCharge(?int $charge): static
+    {
+        $this->charge = $charge;
+
+        return $this;
+    }
+
+    public function getDuree(): ?\DateTimeImmutable
+    {
+        return $this->duree;
+    }
+
+    public function setDuree(\DateTimeImmutable $duree): static
+    {
+        $this->duree = $duree;
+
+        return $this;
+    }
+}
