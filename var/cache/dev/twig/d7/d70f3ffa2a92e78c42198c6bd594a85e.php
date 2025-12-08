5ffc0bae671220b5011c3ff90d0288935493c2e5
Théo <theo@gmail.com>
2025-12-08 11:00:41 +0400

    Ajout partie Objectif et Seance route création, visualisation, etc

--- var/cache/dev/twig/d7/d70f3ffa2a92e78c42198c6bd594a85e.php
+++ var/cache/dev/twig/d7/d70f3ffa2a92e78c42198c6bd594a85e.php
@@ -61,18 +61,16 @@ class __TwigTemplate_3b09d9687286c4e600dd425325ff9ae4 extends Template
         ";
         // line 7
         yield from $this->unwrap()->yieldBlock('stylesheets', $context, $blocks);
-        // line 15
-        yield "            <h1> Test </h1>
-        ";
-        // line 16
+        // line 23
+        yield "        ";
         yield from $this->unwrap()->yieldBlock('javascripts', $context, $blocks);
-        // line 18
+        // line 25
         yield "    </head>
     <body>
         ";
-        // line 20
+        // line 27
         yield from $this->unwrap()->yieldBlock('body', $context, $blocks);
-        // line 21
+        // line 28
         yield "    </body>
 </html>
 ";
@@ -122,27 +120,37 @@ class __TwigTemplate_3b09d9687286c4e600dd425325ff9ae4 extends Template
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "stylesheets"));
 
         // line 8
-        yield "            <link rel=\"stylesheet\" href=\"";
+        yield "         ";
+        // line 9
+        yield "            <link
+                href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css\"
+                rel=\"stylesheet\"
+                integrity=\"sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH\"
+                crossorigin=\"anonymous\"
+            >
+            
+            <link rel=\"stylesheet\" href=\"";
+        // line 16
         yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape($this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("css/base.css"), "html", null, true);
         yield "\">
             <link rel=\"stylesheet\" href=\"";
-        // line 9
+        // line 17
         yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape($this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("css/layout.css"), "html", null, true);
         yield "\">
             <link rel=\"stylesheet\" href=\"";
-        // line 10
+        // line 18
         yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape($this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("css/components.css"), "html", null, true);
         yield "\">
             <link rel=\"stylesheet\" href=\"";
-        // line 11
+        // line 19
         yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape($this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("css/forms.css"), "html", null, true);
         yield "\">
             <link rel=\"stylesheet\" href=\"";
-        // line 12
+        // line 20
         yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape($this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("css/pages.css"), "html", null, true);
         yield "\">
             <link rel=\"stylesheet\" href=\"";
-        // line 13
+        // line 21
         yield $this->env->getRuntime('Twig\Runtime\EscaperRuntime')->escape($this->extensions['Symfony\Bridge\Twig\Extension\AssetExtension']->getAssetUrl("css/utilities.css"), "html", null, true);
         yield "\">
         ";
@@ -155,7 +163,7 @@ class __TwigTemplate_3b09d9687286c4e600dd425325ff9ae4 extends Template
         yield from [];
     }
 
-    // line 16
+    // line 23
     /**
      * @return iterable<null|scalar|\Stringable>
      */
@@ -168,7 +176,7 @@ class __TwigTemplate_3b09d9687286c4e600dd425325ff9ae4 extends Template
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f = $this->extensions["Symfony\\Bridge\\Twig\\Extension\\ProfilerExtension"];
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->enter($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof = new \Twig\Profiler\Profile($this->getTemplateName(), "block", "javascripts"));
 
-        // line 17
+        // line 24
         yield "        ";
         
         $__internal_6f47bbe9983af81f1e7450e9a3e3768f->leave($__internal_6f47bbe9983af81f1e7450e9a3e3768f_prof);
@@ -179,7 +187,7 @@ class __TwigTemplate_3b09d9687286c4e600dd425325ff9ae4 extends Template
         yield from [];
     }
 
-    // line 20
+    // line 27
     /**
      * @return iterable<null|scalar|\Stringable>
      */
@@ -214,7 +222,7 @@ class __TwigTemplate_3b09d9687286c4e600dd425325ff9ae4 extends Template
      */
     public function getDebugInfo(): array
     {
-        return array (  183 => 20,  172 => 17,  159 => 16,  146 => 13,  142 => 12,  138 => 11,  134 => 10,  130 => 9,  125 => 8,  112 => 7,  89 => 5,  76 => 21,  74 => 20,  70 => 18,  68 => 16,  65 => 15,  63 => 7,  58 => 5,  52 => 1,);
+        return array (  191 => 27,  180 => 24,  167 => 23,  154 => 21,  150 => 20,  146 => 19,  142 => 18,  138 => 17,  134 => 16,  125 => 9,  123 => 8,  110 => 7,  87 => 5,  74 => 28,  72 => 27,  68 => 25,  65 => 23,  63 => 7,  58 => 5,  52 => 1,);
     }
 
     public function getSourceContext(): Source
@@ -226,6 +234,14 @@ class __TwigTemplate_3b09d9687286c4e600dd425325ff9ae4 extends Template
         <title>{% block title %}Welcome!{% endblock %}</title>
         <link rel=\"icon\" href=\"data:image/svg+xml,<svg xmlns=%22http://www.w3.org/2000/svg%22 viewBox=%220 0 128 128%22><text y=%221.2em%22 font-size=%2296%22>⚫️</text><text y=%221.3em%22 x=%220.2em%22 font-size=%2276%22 fill=%22%23fff%22>sf</text></svg>\">
         {% block stylesheets %}
+         {# === AJOUT ICI === #}
+            <link
+                href=\"https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css\"
+                rel=\"stylesheet\"
+                integrity=\"sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH\"
+                crossorigin=\"anonymous\"
+            >
+            
             <link rel=\"stylesheet\" href=\"{{ asset('css/base.css') }}\">
             <link rel=\"stylesheet\" href=\"{{ asset('css/layout.css') }}\">
             <link rel=\"stylesheet\" href=\"{{ asset('css/components.css') }}\">
@@ -233,7 +249,6 @@ class __TwigTemplate_3b09d9687286c4e600dd425325ff9ae4 extends Template
             <link rel=\"stylesheet\" href=\"{{ asset('css/pages.css') }}\">
             <link rel=\"stylesheet\" href=\"{{ asset('css/utilities.css') }}\">
         {% endblock %}
-            <h1> Test </h1>
         {% block javascripts %}
         {% endblock %}
     </head>
