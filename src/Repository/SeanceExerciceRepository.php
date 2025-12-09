2eec7197f287388d57ea2ae1044422273dfa321a
Théo <theo@gmail.com>
2025-12-09 14:15:29 +0400

    Ajout partie statistique + style

--- src/Repository/SeanceExerciceRepository.php
+++ src/Repository/SeanceExerciceRepository.php
@@ -16,6 +16,44 @@ class SeanceExerciceRepository extends ServiceEntityRepository
         parent::__construct($registry, SeanceExercice::class);
     }
 
+    public function findForUserBetweenDates($user, \DateTimeInterface $start, \DateTimeInterface $end): array
+    {
+        // retourne les SeanceExercice liés aux séances du user entre les dates
+        $qb = $this->createQueryBuilder('se')
+            ->innerJoin('se.seances', 's')
+            ->addSelect('s')
+            ->where('s.user = :user')
+            ->andWhere('s.date_entrainement BETWEEN :start AND :end')
+            ->setParameter('user', $user)
+            ->setParameter('start', $start)
+            ->setParameter('end', $end)
+            ->orderBy('s.date_entrainement', 'ASC')
+            ->getQuery();
+
+        return $qb->getResult();
+    }
+
+    public function findForUserAndExerciseBetweenDates($user, int $exerciseId, \DateTimeInterface $start, \DateTimeInterface $end): array
+    {
+        $qb = $this->createQueryBuilder('se')
+            ->innerJoin('se.seances', 's')
+            ->addSelect('s')
+            ->innerJoin('se.exercices', 'e')
+            ->addSelect('e')
+            ->where('s.user = :user')
+            ->andWhere('e.id = :exId')
+            ->andWhere('s.date_entrainement BETWEEN :start AND :end')
+            ->setParameter('user', $user)
+            ->setParameter('exId', $exerciseId)
+            ->setParameter('start', $start)
+            ->setParameter('end', $end)
+            ->orderBy('s.date_entrainement', 'ASC')
+            ->getQuery();
+
+        return $qb->getResult();
+    }
+
+
     //    /**
     //     * @return SeanceExercice[] Returns an array of SeanceExercice objects
     //     */
