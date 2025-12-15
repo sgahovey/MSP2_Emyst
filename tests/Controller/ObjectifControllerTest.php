46cd8e3b33c44c596fbd2a3a4e6ba272d336647b
AHOVEY Saint-George <sgahovey@gmail.com>
2025-12-15 10:51:49 +0400

    Correction assertions

--- tests/Controller/ObjectifControllerTest.php
+++ tests/Controller/ObjectifControllerTest.php
@@ -130,8 +130,8 @@ final class ObjectifControllerTest extends WebTestCase
         $this->client->followRedirects(false);
         $this->client->submit($form);
 
-        // Vérifier la redirection après soumission
-        self::assertResponseRedirects($this->path);
+        // Vérifier la redirection après soumission (vers /objectif sans slash final)
+        self::assertResponseRedirects('/objectif');
 
         $updated = $this->manager->getRepository(Objectif::class)->find($objectif->getId());
 
